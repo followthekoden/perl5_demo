6e52a77c98547901a8a7b07f159e408c19fe2bc0
Dallas Baker <dallas.c.baker@dalbaker.com>
2017-11-21 03:59:06 -0800

    added description

--- reading_logs.pl
+++ reading_logs.pl
@@ -3,6 +3,8 @@ use warnings;
 
 $| = 1;
 
+#Cyber Analyst can use this look through secuirty logs of systems.
+#this is a template meant to be built upon.
 sub read_log {
     my $log = 'linux_user_auth.log';
     open( INPUT, $log, ) or die("input file $log not found");
